6f106c816f7ab560c5030b68ba543e3124d08615
isaactls <isacktolesa@gmail.com>
2024-04-13 08:29:26 +0300

    fix

--- portfolio_main/script.js
+++ portfolio_main/script.js
@@ -1,24 +1,16 @@
-let bdy = document.querySelector('body')
-let mode = document.querySelector('.mode')
+let bdy = document.querySelector("body");
+let mode = document.querySelector(".mode");
 let check = false;
-mode.addEventListener('click', ()=>{
-    bdy.classList.toggle('background')
-    check = !check
-    let img = document.createElement('img')
-    if(check){
-        img.setAttribute('src', './image/night.png')
-        img.setAttribute('width', '30px')
-        mode.innerHTML = "";
-        mode.appendChild(img)
-    }
-    else{
-        img.setAttribute('src', './image/light.png')
-        img.setAttribute('width', '40px')
-        mode.innerHTML = "";
-        mode.appendChild(img)
-    }
-})
-const messages = () =>{
-alert('contact me to get email and resume info ')
-}
-
+mode.addEventListener("click", () => {
+  bdy.classList.toggle("background");
+  check = !check;
+  let img = document.querySelector(".mode img");
+  if (check) {
+    img.setAttribute("src", "./image/night.png");
+  } else {
+    img.setAttribute("src", "./image/light.png");
+  }
+});
+const messages = () => {
+  alert("contact me to get email and resume info ");
+};
